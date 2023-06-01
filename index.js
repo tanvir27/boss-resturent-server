908cce2dc6be4e1dfb58f607f0f25bdb07de29ff
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-06-02 00:55:07 +0600

    delete operation implemented

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hxmdm1d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,6 +42,17 @@ async function run() {
     });
 
     //cartCollection 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if (!email) {
+        res.send([])
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray()
+      res.send(result);
+    })
+
     app.post('/carts', async (req, res) => {
       const item = req.body;
       console.log(item);
@@ -49,7 +60,28 @@ async function run() {
       res.send(result);
     })
 
+    //delete 
+    // app.delete('/carts/:id', async (req, res) => { 
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await cartCollection.deleteOne(query);
+    //   res.send(result);
+    // })
 
+    app.delete("/carts/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.error("Error deleting document:", error);
+        res
+          .status(500)
+          .send({ error: "An error occurred while deleting the document." });
+      }
+    });
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
