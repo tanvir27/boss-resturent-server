a38da99d0646be66bc5b01e92e96b52b862ef019
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-06-09 02:13:03 +0600

    server with jwt and admnin

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// authorization verify jwt
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    // console.log(decoded.foo);
+
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "Unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hxmdm1d.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,11 +50,87 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
-    //menu 
+    // jwt authorization
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // verifyAdmin
+    const verifyAdmin = async (req, res, next) => { 
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({error: true, message: 'forbidden message'});
+      }
+      next();
+    }
+
+    /*
+    * secure = use JWT token
+
+    */
+
+    // users related all api
+
+    app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      console.log("existing User", existingUser);
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // security layer : verify JWT
+    // email same
+    // check admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    //update
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    //menu
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
@@ -41,27 +142,36 @@ async function run() {
       res.send(result);
     });
 
-    //cartCollection 
-    app.get('/carts', async (req, res) => {
+    //cart Collection apis
+    app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
-      console.log(email);
-      if (!email) {
-        res.send([])
-      }
-      const query = { email: email };
-      const result = await cartCollection.find(query).toArray()
-      res.send(result);
-    })
+      // console.log(email);
 
-    app.post('/carts', async (req, res) => {
+      if (!email) {
+        res.send([]);
+      }
+
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "Forbidden Access" });
+      }
+
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
       const item = req.body;
       console.log(item);
-      const result = await cartCollection.insertOne(item)
+      const result = await cartCollection.insertOne(item);
       res.send(result);
-    })
+    });
 
-    //delete 
-    // app.delete('/carts/:id', async (req, res) => { 
+    //delete
+    // app.delete('/carts/:id', async (req, res) => {
     //   const id = req.params.id;
     //   const query = { _id: new ObjectId(id) };
     //   const result = await cartCollection.deleteOne(query);
@@ -81,7 +191,7 @@ async function run() {
           .send({ error: "An error occurred while deleting the document." });
       }
     });
-    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -103,18 +213,18 @@ app.listen(port, () => {
 });
 
 /**
- * 
+ *
  * ------------------------------------------------
- *                NAMING CONVENTION 
- *------------------------------------------------- 
+ *                NAMING CONVENTION
+ *-------------------------------------------------
  * users: userCollection
  * app.get('/users) == more than one needs then its use
  * app.get('/users/:id')
- * app.post('/users') -- create 
+ * app.post('/users') -- create
  * app.put('/users/:id')
  * app.patch('/users/:id')
  * app.delete('/users/:id')
- * 
- * 
- *  
+ *
+ *
+ *
  * */
